8304c2cc5d9a70800f5b1921bc65d75e5be50ec0
Vincent de RIBOU <v.debossoreillederibou@akka.eu>
2019-06-19 19:31:53 +0200

    Add option to choose Qt4 or Qt5
    - generates libs and bins onto root dir.

--- qt_compliancy.cmake
+++ qt_compliancy.cmake
@@ -1,25 +1,28 @@
 
-# if("$ENV{ROS_DISTRO}" STREQUAL "indigo") 
+option(FORCE_QT4 "Force using Qt4 instead of Qt5" OFF)
 
-#     find_package(Qt4 REQUIRED QtCore QtNetwork)
-#     include(${QT_USE_FILE})
-#     add_definitions( ${QT_DEFINITIONS} )
-#     
-#     macro(CUSTOM_QT_WRAP_CPP )
-#         QT4_WRAP_CPP(${ARGN})
-#     endmacro()
-#     
-#     set(CUSTOM_QT_LIBRARIES ${QT_LIBRARIES})
+if(${FORCE_QT4}) 
+    message(STATUS "Qt4 usage forced by option input")
+    find_package(Qt4 REQUIRED QtCore QtNetwork)
+    include(${QT_USE_FILE})
+    add_definitions( ${QT_DEFINITIONS} )
     
-# elseif("$ENV{ROS_DISTRO}" STREQUAL "kinetic") 
+    macro(CUSTOM_QT_WRAP_CPP )
+        QT4_WRAP_CPP(${ARGN})
+    endmacro()
+    
+    set(CUSTOM_QT_LIBRARIES ${QT_LIBRARIES})
+    
+else() 
+    message(STATUS "Qt5 usage by default, use FORCE_QT4 option for alternate choice")
 
     find_package(Qt5 REQUIRED Core)
     
     include_directories(${Qt5Core_INCLUDE_DIRS})
     add_definitions(${Qt5Core_DEFINITIONS})
     
-#     include_directories(${Qt5Network_INCLUDE_DIRS})
-#     add_definitions(${Qt5Network_DEFINITIONS})
+    include_directories(${Qt5Network_INCLUDE_DIRS})
+    add_definitions(${Qt5Network_DEFINITIONS})
     
     macro(CUSTOM_QT_WRAP_CPP )
         QT5_WRAP_CPP(${ARGN})
@@ -27,7 +30,5 @@
     
     set(CUSTOM_QT_LIBRARIES ${Qt5Core_LIBRARIES} ${Qt5Network_LIBRARIES})
 
-# else()
-#     message(FATAL_ERROR "ROS version not set!!")
-# endif()
+endif()
 
