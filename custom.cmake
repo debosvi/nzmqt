146a70ab4c643a389e0e0f909bd5395bc5e429cb
Vincent de RIBOU <v.deribou@gmail.com>
2019-06-24 19:27:39 +0200

    #1: Use pkg-config to find zmq stuffs.

--- custom.cmake
+++ custom.cmake
@@ -3,3 +3,22 @@ set(root_output_dir ${CMAKE_BINARY_DIR}/.built)
 
 SET(EXECUTABLE_OUTPUT_PATH ${root_output_dir}/bin)
 SET(LIBRARY_OUTPUT_PATH ${root_output_dir}/lib)
+
+list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake")
+
+find_package(PkgConfig)
+
+## use pkg-config to get hints for 0mq locations
+pkg_check_modules(PC_ZeroMQ QUIET zmq)
+
+## use the hint from above to find where 'zmq.hpp' is located
+find_path(ZeroMQ_INCLUDE_DIR
+        NAMES zmq.hpp
+        PATHS ${PC_ZeroMQ_INCLUDE_DIRS}
+        )
+
+## use the hint from about to find the location of libzmq
+find_library(ZeroMQ_LIBRARY
+        NAMES zmq
+        PATHS ${PC_ZeroMQ_LIBRARY_DIRS}
+        )
